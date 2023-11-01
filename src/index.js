ed2d280152713807b1644c0bc50a94246f2403f0
YuriyNefedoff <y.nefedoff.wm@gmail.com>
2023-11-01 11:51:17 +0300

    createRoot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello every buddy');
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
+
+const el = <div>Hello every buddy</div>
+const root = createRoot(document.getElementById('root'))
+root.render(el)
+console.log(123123)
