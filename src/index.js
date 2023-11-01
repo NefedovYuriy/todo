cac4341e444967d00e752ebcad81c2d6a80cb8a5
YuriyNefedoff <y.nefedoff.wm@gmail.com>
2023-11-01 14:45:43 +0300

    createElemenApp_SearchPanel_TodoList

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-const el = <div>Hello every buddy</div>
+const TodoList = () => {
+    return(
+        <ul>
+            <li>Learn React</li>
+            <li>Create Awesome App</li>
+            <li>Sell Awesome App</li>
+        </ul>
+    )
+}
+
+const AppHeader = () => {
+    return <h1>My Todo List</h1>
+    
+}
+
+const SearchPanel = () => {
+    return <input placeholder='search'/>
+}
+
+const App = () => {
+    return (
+    <div>
+        <AppHeader/>
+        <SearchPanel/>
+        <TodoList/>
+    </div>
+    )
+}
+
 const root = createRoot(document.getElementById('root'))
-root.render(el)
-console.log(123123)
+root.render(<App/>)
